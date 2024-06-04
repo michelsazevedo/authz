c511ed10087206db0a4a71acbb325276647ec27a
Michel Azevedo <michel.azevedos@gmail.com>
2024-06-03 23:57:23 -0300

    :whale: Wired up Structured Logging

--- main.go
+++ main.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/michelsazevedo/authz/config"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	e := echo.New()
 	conf, _ := config.NewConfig()
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	e := echo.New()
+	e.Use(middleware.Recover())
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:    true,
+		LogStatus: true,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			log.Info().
+				Str("URI", v.URI).
+				Int("status", v.Status).
+				Msg("Method=" + c.Request().Method)
+
+			return nil
+		},
+	}))
 
 	e.GET("/healthz", func(c echo.Context) error {
 		healthz := map[string]int{"status": 200}
